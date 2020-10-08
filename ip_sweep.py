cbd3bd580969d679f4763d933d8ba7bae7b747e2
Maram Alfawaz <46841150+MramAlfawaz@users.noreply.github.com>
2020-10-09 01:18:44 +0300

    Update ip_sweep.py

--- ip_sweep.py
+++ ip_sweep.py
@@ -2,17 +2,16 @@
 
 import os   
 
-#and then check the response...
+#replace ip address with your ip
 ip = "0"
-hostname = "10.11.1." + str(ip)
-#response = os.system("ping -c 1 " + hostname)
+ips = "10.11.1." + str(ip)
 
 for ip in range(255+1) :
- hostname = "10.11.1." + str(ip)
- response = os.system("ping -c 1 " + hostname )
+ ips = "10.11.1." + str(ip)
+ response = os.system("ping -c 1 " + ips )
 
  if response == 0:    
 
-  print (hostname, 'is up!')
+  print (ips, 'is up!')
  else:
-  print (hostname, 'is down!')
+  print (ips, 'is down!')
